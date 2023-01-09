63c2bc7aaa9a9275f25637f1e14ac533980fd8fa
Tomás Salina <salinatomass53@gmail.com>
2023-01-09 14:40:11 -0300

    learning: controllers and params

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,23 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('products')
+  getProducts() {
+    return 'products';
+  }
+
+  @Get('products/:productId')
+  getProduct(@Param('productId') id: string) {
+    return `Product #${id}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategories(
+    @Param('id') id: string,
+    @Param('productId') productId: string,
+  ) {
+    return `Este es el producto #${productId} de la categoría #${id}`;
   }
 }
