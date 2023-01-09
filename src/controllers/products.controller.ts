7d79870c3d5a8f50b7cafc1b562d61948b54347e
Tomás Salina <salinatomass53@gmail.com>
2023-01-09 16:21:45 -0300

    feat(products): add status code

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -7,6 +7,8 @@ import {
   Body,
   Put,
   Delete,
+  HttpStatus,
+  HttpCode,
 } from '@nestjs/common';
 
 @Controller('products')
@@ -26,6 +28,7 @@ export class ProductsController {
   }
 
   @Get(':productId')
+  @HttpCode(HttpStatus.ACCEPTED)
   getOne(@Param('productId') id: string) {
     return {
       id,
